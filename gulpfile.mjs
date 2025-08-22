edaf6f287f1f6870c3fb19943e4b5b734dbb99a3
Juha Suni <juha.suni@showell.com>
2025-08-22 17:39:26 +0300

    Change package visibility to restricted

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -2335,7 +2335,7 @@ function packageJson() {
     version: VERSION,
     publishConfig: {
       registry: "https://npm.pkg.github.com/",
-      access: "public",
+      access: "restricted",
     },
     main: "build/pdf.mjs",
     types: "types/src/pdf.d.ts",
